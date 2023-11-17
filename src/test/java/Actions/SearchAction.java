4526d90006addd585047fdbebc0f9e221b6afd54
pravin-code <pravinkannan9865@gmail.com>
2023-11-17 18:25:16 +0530

    pravin 2nd commit

--- src/test/java/Actions/SearchAction.java
+++ src/test/java/Actions/SearchAction.java
@@ -1,21 +1,67 @@
 package Actions;
 
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
 
 import Locators.SearchLocator;
 import Utility.HelperClass;
 
 public class SearchAction {
-static SearchLocator searchlocator = null;
-	
+	static SearchLocator searchlocator = null;
+	static Actions act = new Actions(HelperClass.getDriver());
+	static JavascriptExecutor js = ((JavascriptExecutor) HelperClass.getDriver()); 
+
 	public SearchAction() {
-		this.searchlocator= new SearchLocator();
+		this.searchlocator = new SearchLocator();
 		PageFactory.initElements(HelperClass.getDriver(), searchlocator);
 	}
+
+	public static void setSearch(String product) {
+		searchlocator.search.sendKeys(product);
+	}
+
+	public void entbutton() throws AWTException {
+		Robot rob = new Robot();
+		rob.keyPress(KeyEvent.VK_ENTER);
+		rob.keyRelease(KeyEvent.VK_ENTER);
+	}
+
+	public static void frameshape() {
+		act.moveToElement(searchlocator.frameshape).click().build().perform();
+		
+	}
+
+	public static void framesize() {
+		act.moveToElement(searchlocator.framesize).click().build().perform();
+		
+	}
+
+	public static void frametype() {
+		act.moveToElement(searchlocator.frametype).click().build().perform();
+	}
+
 	
-	public static void setSearch(String product, Keys enter) {
-          searchlocator.search.sendKeys(product);
+
+	public static void searchsort() {
+		Select select = new Select(searchlocator.searchsort);
+		select.selectByIndex(2);
+	}
+
+	public static void product() {
+		searchlocator.product.click();
+	}
+
+	public static void lens() {
+		searchlocator.lens.click();
 	}
 
 }
--- src/test/java/Definitions/SunglassDefinition.java
+++ src/test/java/Definitions/SunglassDefinition.java
@@ -11,49 +11,37 @@ import io.cucumber.java.en.When;
 
 public class SunglassDefinition {
 	SunglassAction sunact = new SunglassAction();
-	
 
-@Given("launch the webpage of lenskart")
-public void launch_the_webpage_of_lenskart() {
-	HelperClass.openPage("https://www.lenskart.com/");
-	String parentWindow= HelperClass.getDriver().getWindowHandle();
-}
-	
-	@When("Hover the mouse to sunglasses")
-	public void hover_the_mouse_to_sunglasses() {
-	    sunact.sunglass();
+	@Given("launch the webpage of lenskart")
+	public void launch_the_webpage_of_lenskart() {
+		HelperClass.openPage("https://www.lenskart.com/");
+		String parentWindow = HelperClass.getDriver().getWindowHandle();
 	}
 
-	@When("Hover the mouse to the men section")
-	public void hover_the_mouse_to_the_men_section() {
-		sunact.men();
-	}
-
-	@When("hover the mouse to the classic sunglasses option and click")
-	public void hover_the_mouse_to_the_classic_sunglasses_option_and_click() {
-		sunact.classic();
+	@When("Hover the mouse to sunglasses and click")
+	public void hover_the_mouse_to_sunglasses_and_click() {
+		sunact.sunglass();
 	}
 
 	@When("check the variety of sunglasss")
 	public void check_the_variety_of_sunglasss() {
 		sunact.range();
 	}
-	
+
 	@When("click the range")
 	public void click_the_range() {
 		sunact.seller();
-		
-		
+
 	}
 
 	@When("Sort the product by price:low to high")
 	public void sort_the_product_by_price_low_to_high() {
 		sunact.sort();
-		
+
 	}
 
-	@When("Filter the frame type by full rim")
-	public void filter_the_frame_type_by_full_rim() {
+	@When("Filtering  frame type by half rim")
+	public void filtering_frame_type_by_half_rim() {
 		sunact.filter();
 	}
 
@@ -65,39 +53,34 @@ public void launch_the_webpage_of_lenskart() {
 	@When("switch to another tab")
 	public void switch_to_another_tab() {
 		Set<String> allWindows = HelperClass.getDriver().getWindowHandles();
-		for(String curWindow : allWindows){
+		for (String curWindow : allWindows) {
 			HelperClass.getDriver().switchTo().window(curWindow);
 		}
 	}
 
 	@When("click the power")
 	public void click_the_power() {
-		//sunact.power();
+		sunact.power();
 	}
 
 	@When("choose the color for sunglass")
 	public void choose_the_color_for_sunglass() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+		sunact.lenscolor();
 	}
 
 	@When("click the continue")
 	public void click_the_continue() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+		sunact.contbutton();
 	}
 
 	@When("user given the checkout")
 	public void user_given_the_checkout() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+		sunact.ckeckout();
 	}
 
 	@Then("asserting the bill details")
 	public void asserting_the_bill_details() {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new io.cucumber.java.PendingException();
+		sunact.bill();
 	}
-
 
 }
