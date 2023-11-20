1b881b6c610c7777bc669e8d0f37ceb47703ce11
pravin-code <pravinkannan9865@gmail.com>
2023-11-20 09:53:19 +0530

    pravin 3rd commit

--- src/test/java/Definitions/SunglassDefinition.java
+++ src/test/java/Definitions/SunglassDefinition.java
@@ -11,35 +11,36 @@ import io.cucumber.java.en.When;
 
 public class SunglassDefinition {
 	SunglassAction sunact = new SunglassAction();
+	
 
-	@Given("launch the webpage of lenskart")
-	public void launch_the_webpage_of_lenskart() {
-		HelperClass.openPage("https://www.lenskart.com/");
-		String parentWindow = HelperClass.getDriver().getWindowHandle();
-	}
-
-	@When("Hover the mouse to sunglasses and click")
-	public void hover_the_mouse_to_sunglasses_and_click() {
-		sunact.sunglass();
-	}
+@Given("launch the webpage of lenskart")
+public void launch_the_webpage_of_lenskart() {
+	HelperClass.openPage("https://www.lenskart.com/");
+	String parentWindow= HelperClass.getDriver().getWindowHandle();
+}
+	
+@When("Hover the mouse to sunglasses and click")
+public void hover_the_mouse_to_sunglasses_and_click() {
+    sunact.sunglass();}
 
 	@When("check the variety of sunglasss")
 	public void check_the_variety_of_sunglasss() {
 		sunact.range();
 	}
-
+	
 	@When("click the range")
 	public void click_the_range() {
 		sunact.seller();
-
+		
+		
 	}
 
 	@When("Sort the product by price:low to high")
 	public void sort_the_product_by_price_low_to_high() {
 		sunact.sort();
-
+		
 	}
-
+	
 	@When("Filtering  frame type by half rim")
 	public void filtering_frame_type_by_half_rim() {
 		sunact.filter();
@@ -53,7 +54,7 @@ public class SunglassDefinition {
 	@When("switch to another tab")
 	public void switch_to_another_tab() {
 		Set<String> allWindows = HelperClass.getDriver().getWindowHandles();
-		for (String curWindow : allWindows) {
+		for(String curWindow : allWindows){
 			HelperClass.getDriver().switchTo().window(curWindow);
 		}
 	}
@@ -75,12 +76,13 @@ public class SunglassDefinition {
 
 	@When("user given the checkout")
 	public void user_given_the_checkout() {
-		sunact.ckeckout();
+	    sunact.ckeckout();
 	}
 
 	@Then("asserting the bill details")
 	public void asserting_the_bill_details() {
-		sunact.bill();
+	   sunact.bill();
 	}
+
 
 }
