904f698692b781898e24f1205864e4abf2290f07
Dinesh Mulinti <dinesh.mulinti@expleogroup.com>
2023-11-21 10:15:43 +0530

    new

--- src/test/java/Runner/Runner.java
+++ src/test/java/Runner/Runner.java
@@ -5,8 +5,8 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
  
 @RunWith(Cucumber.class)
-@CucumberOptions(tags="",features="src\\test\\resources\\Feature\\categorysunglass.feature",glue ="Definitions",plugin= {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"})
-
+//@CucumberOptions(tags="",features="src\\test\\resources\\Feature\\categorysunglass.feature",glue ="Definitions",plugin= {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"})
+@CucumberOptions(tags="",features="src\\test\\resources\\Feature\\Register.feature",glue ="Definitions",plugin= {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"})
  
 
 public class Runner {
