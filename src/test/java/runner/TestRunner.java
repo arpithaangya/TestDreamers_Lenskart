87cf64e43e5028b642cae60efd9fa093b8570761
Arpitha Shekhar Angya <arpitha.shekharangya@expleogroup.com>
2023-11-29 11:05:02 +0530

    arpi add

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -5,7 +5,7 @@ import org.testng.annotations.DataProvider;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(tags="",features={"src\\test\\resources\\Feature\\FollowUs.feature","src\\test\\resources\\Feature\\Login.feature","src\\test\\resources\\Feature\\KidsGlasses.feature"},glue ="Definitions",plugin= {"pretty", "html:target/Assessment-reports/Cucumber.html", "junit:target/Assessment-reports/Cucumber.xml", "json:target/Assessment-reports/Cucumber.json","rerun:target/failedrerun.txt"})
+@CucumberOptions(tags="",features={"src\\test\\resources\\Feature"},glue ="Definitions",plugin= {"pretty", "html:target/Assessment-reports/Cucumber.html", "junit:target/Assessment-reports/Cucumber.xml", "json:target/Assessment-reports/Cucumber.json","rerun:target/failedrerun.txt"})
 
 public class TestRunner extends AbstractTestNGCucumberTests{
 	
